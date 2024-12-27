52ce035998cf7afb425f6ced82fcc5d5521f516b
parkcheayeon <ddaany927@gmail.com>
2024-12-27 22:54:53 +0900

    init commit

---
+++ app/src/main/java/com/chaeyeon/modularization/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.chaeyeon.modularization
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chaeyeon.modularization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
