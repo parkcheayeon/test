ddce1817792c1393e76b9894ab5ce3a19d999398
parkcheayeon <ddaany927@gmail.com>
2024-12-28 20:12:20 +0900

    test commit 2

--- app/src/main/java/com/chaeyeon/modularization/MainActivity.kt
+++ app/src/main/java/com/chaeyeon/modularization/MainActivity.kt
@@ -6,7 +6,7 @@ import com.chaeyeon.modularization.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-// test
+// test 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
